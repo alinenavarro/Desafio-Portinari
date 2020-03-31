0dc5e5abf9dfcdcc2db8d2fd354319430a8226b7
alinenavarro <aline.navarro@totvs.com.br>
2020-03-31 08:44:07 -0300

    alterações nas tarefas e read.me

--- README.md
+++ README.md
@@ -1,27 +1,60 @@
-# Taskapp
+# Desafio Portinari
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.0.
+Aplicativo de tarefas em Angular com Portinari.
 
-## Development server
+### Instalação
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+Instalar as dependências
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+```
+npm install
+```
 
-## Build
+Subir Fake API json. A API estará disponível no endereço: `http://localhost:3000/`
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+```
+json-server db.json
 
-## Running unit tests
+```
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Subir o servidor de aplicação. Estará disponível no endereço: `http://localhost:4200/`
 
-## Running end-to-end tests
+```
+ng serve
+```
 
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+## Acesso
 
-## Further help
+Efetuar login:
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+* Usuário: teste@email.com
+* Senha: 12345
+
+
+## Certificados
+
+[HTTP: Entendendo a web por baixo dos panos](https://cursos.alura.com.br/certificate/17913e77-f5e1-4e13-aae5-7d5fdd43064a)
+
+[HTML5 e CSS3 parte 1: A primeira página da Web](https://cursos.alura.com.br/certificate/499bd763-dafd-43c2-8c3f-b97954f23a57)
+
+[HTML5 e CSS3 parte 2: Posicionamento, listas e navegação](https://cursos.alura.com.br/certificate/8c6ec1b2-7514-40b6-a020-07a354d41f50)
+
+[JavaScript: Programando na linguagem da web](https://cursos.alura.com.br/certificate/a420b76d-8f7c-4c5e-a800-ead8c8d019b3)
+
+[JavaScript avançado I: ES6, orientação a objetos e padrões de projetos](https://cursos.alura.com.br/certificate/d59e18cc-ec57-48d3-a204-f0acc21ec1b8)
+
+[TypeScript parte 2: Mais técnicas e boas práticas](https://cursos.alura.com.br/certificate/bc4f2648-ac32-4dea-a741-06e39f36eb20)
+
+[Angular parte 1: Fundamentos](https://cursos.alura.com.br/certificate/8a168628-4503-4385-a759-7219519bb096)
+
+[Angular parte 2: Autenticação, Forms e lazy loading](https://cursos.alura.com.br/certificate/233adda3-90ec-43a8-ab74-e108a4dab269)
+
+[Angular parte 3: upload, build e novos componentes](https://cursos.alura.com.br/certificate/3e292897-e749-44d7-b552-6bcea78f0b46)
+
+[Angular parte 4: lapidando o projeto](https://cursos.alura.com.br/certificate/96711a69-93b7-45e5-b54a-22a5fb1f4dda)
+
+[Jasmine: Testes automatizados em JavaScript](https://cursos.alura.com.br/certificate/1b5abfe0-8e27-4d25-aa7f-e7e53c5ef1c2)
+
+
+
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/interfaces/Task';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { TaskService } from 'src/app/tasks/task.service';
+import { PoMenuItem } from '@portinari/portinari-ui';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -24,42 +26,17 @@ import { TaskService } from 'src/app/tasks/task.service';
   ]
 })
 export class TasksComponent implements OnInit {
-  // tasks: Task[];
-  taskTitle: string;
-  // idForTask: number; //retirar
-  // beforeEditCache: string;
-  // filter: string;
-  // anyRemainingModel: boolean;
 
-  //pesquisar uso de injectable aqui!!!!!!!
-  constructor(private taskService: TaskService) { }
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Logout' , action: this.onClick.bind(this), icon: 'po-icon-home', shortLabel: 'Início' },
+  ];
+
+  taskTitle: string;
+
+  constructor(private taskService: TaskService, private router: Router) { }
 
   ngOnInit() {
-    // this.filter = 'all';
-    // this.anyRemainingModel = true;
-    // this.beforeEditCache = '';
     this.taskTitle = '';
-    // this.idForTask = 4; //retirar
-    // this.tasks = [
-    //   {
-    //     'id': 1,
-    //     'title': 'Finalizar curso de angular',
-    //     'completed': false,
-    //     'editing': false,
-    //   },
-    //   {
-    //     'id': 2,
-    //     'title': 'Subir projeto',
-    //     'completed': false,
-    //     'editing': false,
-    //   },
-    //   {
-    //     'id': 3,
-    //     'title': 'Subir certificados',
-    //     'completed': false,
-    //     'editing': false,
-    //   },
-    // ];
   }
 
   addTask(): void {
@@ -69,73 +46,13 @@ export class TasksComponent implements OnInit {
       }
   
       this.taskService.addTask(this.taskTitle);
-    // this.tasks.push({
-    //   id: this.idForTask, // substituir pelo id do backend
-    //   title: this.taskTitle,
-    //   completed: false,
-    //   editing: false
-    
-    // })
-
-    this.taskTitle = '';
+      this.taskTitle = '';
     // this.idForTask++;
   }
 
-  // editTask(task: Task): void {
-  //   this.beforeEditCache = task.title;   
-  //   task.editing = true;
-  // }
-
-  // doneEdit(task: Task): void {
-  //   if (task.title.trim().length == 0) { //alterei
-  //     task.title = this.beforeEditCache;
-  //   }
-
-  //   task.editing = false;
-  // }
-
-  // cancelEdit(task: Task): void {
-  //   task.title = this.beforeEditCache;
-  //   task.editing = false;
-  // }
-
-  // deleteTask(id: number): void {
-  //   this.tasks = this.tasks.filter(task => task.id !== id);
-  // }
-
-  // remaining(): number {
-  //   return this.tasks.filter(task => !task.completed).length;
-  // }
-
-  // atLeastOneCompleted(): boolean {
-  //   return this.tasks.filter(task => task.completed).length > 0;
-  // }
-
-  // clearCompleted(): void {
-  //   this.tasks = this.tasks.filter(task => !task.completed);
-  // }
-
-  // checkAllTasks(): void {
-  //   this.tasks.forEach(task => task.completed = 
-  //     (<HTMLInputElement>event.target).checked)
-  //     this.anyRemainingModel = this.anyRemaining();
-  // }
-
-  // anyRemaining(): boolean {
-  //   return this.remaining() !== 0;
-  // }
-
-  // tasksFiltered(): Task[] {
-  //   if (this.filter == 'all') {
-  //     return this.tasks
-  //   } else if (this.filter == 'active') {
-  //     return this.tasks.filter(task => !task.completed)
-  //   } else if (this.filter == 'completed') {
-  //     return this.tasks.filter(task => task.completed)
-  //   }
-
-  //   return this.tasks;
-  // }
+  private onClick() {
+    this.router.navigate(['/login']);
+  }
 
 }
  
